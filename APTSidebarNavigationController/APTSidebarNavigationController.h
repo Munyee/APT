07f20b26ea91385aac73dd8eb9520466be4550f1
Chan Mun Yee <yee_chan22@msn.com>
2019-10-16 14:46:18 +0800

    Initial Commit

---
+++ APTSidebarNavigationController/APTSidebarNavigationController.h
@@ -0,0 +1,19 @@
+//
+//  APTSidebarNavigationController.h
+//  APTSidebarNavigationController
+//
+//  Created by Chan Mun Yee on 16/10/2019.
+//  Copyright © 2019 TT dotCom Sdn Bhd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for APTSidebarNavigationController.
+FOUNDATION_EXPORT double APTSidebarNavigationControllerVersionNumber;
+
+//! Project version string for APTSidebarNavigationController.
+FOUNDATION_EXPORT const unsigned char APTSidebarNavigationControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APTSidebarNavigationController/PublicHeader.h>
+
+
